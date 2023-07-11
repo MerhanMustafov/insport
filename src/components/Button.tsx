12484563c0f4d2208fd21e69606c45310e668b3e
MerhanMustafov <76586375+MerhanMustafov@users.noreply.github.com>
2023-07-11 19:24:52 +0300

    IN-8 project initialization (#1)
    initialize react typescript project with yarn create vite and configured
    eslint typescript prettier and absolute imports with this "@" alias"

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+const Button: React.FC = () => {
+    return (
+        <>
+            <button>Button</button>
+        </>
+    );
+};
+
+export default Button;
---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    resolve: {
+        alias: { "@": path.resolve(__dirname, "./src") }
+    },
+    plugins: [react(), tsconfigPaths()]
+});
