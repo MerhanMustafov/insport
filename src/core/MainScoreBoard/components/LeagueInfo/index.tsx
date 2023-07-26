5fff8ae8e1b3442fc2722cb53acd28ada021201e
MerhanMustafov <76586375+MerhanMustafov@users.noreply.github.com>
2023-07-27 00:35:49 +0300

    IN-15 add LeagueInfo header (#5)
    Added league info section (component) header

--- src/core/MainScoreBoard/components/LeagueInfo/index.tsx
+++ src/core/MainScoreBoard/components/LeagueInfo/index.tsx
@@ -2,18 +2,50 @@ import { useParams } from "react-router-dom";
 
 import styled from "styled-components";
 
+import axiosInstance from "@/lib/axios/axiosConfig";
+import { IAxiosData } from "@/models/api";
+
+import { useQuery } from "@tanstack/react-query";
+
+import LeagueInfoHeader from "./components/LeagueInfoHeader";
+import { ILeague } from "./models/ILeague";
+
 const StyledContainer = styled("div")`
     grid-area: PageScores_LeagueInfo;
 `;
-
+const baseEndPoint = "/leagues?current=true";
 export default function LeagueInfo() {
     const params = useParams();
+    const { data, isLoading, isFetching } = useQuery({
+        queryKey: ["league", params.leagueName],
+        queryFn: getLeagues,
+        enabled: !!params.leagueName
+    });
 
-    console.log(params);
+    async function getLeagues(): Promise<ILeague[]> {
+        const country = params.countryName as string;
+        const league = params.leagueName as string;
+
+        const url = `${baseEndPoint}&country=${country}&name=${league}`;
+        return ((await axiosInstance.get(url)).data as IAxiosData<ILeague[]>).response;
+    }
+    // console.log(params);
+    // console.log("DATA: ", data);
 
     return (
         <StyledContainer>
-            <div>LeagueInfo</div>
+            {(isLoading || isFetching) && <div>Loading ...</div>}
+            {data && data?.length > 0 && (
+                <div>
+                    <LeagueInfoHeader
+                        countryName={data[0].country.name}
+                        flag={data[0].country.flag}
+                        leagueName={data[0].league.name}
+                        logo={data[0].league.logo}
+                    />
+                    <div>LeagueInfo</div>
+                </div>
+            )}
         </StyledContainer>
     );
 }
