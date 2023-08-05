647ff3fcc795c0cd645749eec902abe137cd21dc
Merhan Mustafov <m.o.mustafov@gmail.com>
2023-08-05 12:01:49 +0300

    IN-21 modified PageScores active navigation setting

--- src/pages/PageScores.tsx
+++ src/pages/PageScores.tsx
@@ -1,12 +1,11 @@
 import { useEffect } from "react";
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import styled from "styled-components";
 
 import CountryLeagueList from "@/core/CountryLeagueList";
 import MainScoreBoard from "@/core/MainScoreBoard";
 import { activeLinkObserver } from "@/observers/ActiveLinkObserver";
-import { urlPaths } from "@/routes/urlPaths";
 
 const StyledPageScoreContainer = styled("div")`
      margin: 0 auto;
@@ -30,15 +29,9 @@ const StyleSoccerMain = styled("div")`
      grid-area: Soccer_Main;
 `;
 export default function PageScores() {
-     const location = useLocation();
-
      useEffect(() => {
-          if (location.pathname === urlPaths.HOME) {
-               activeLinkObserver.appNav.notify(null);
-               return;
-          }
           activeLinkObserver.appNav.notify("Scores");
-     }, [location.pathname]);
+     }, []);
 
      return (
           <StyledPageScoreContainer>
