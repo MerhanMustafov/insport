e2da38944b15f7bcf83f307e63730d50f4a38e05
Merhan Mustafov <m.o.mustafov@gmail.com>
2023-08-08 01:10:55 +0300

    IN-12 extracted TeamsCell into its own file

--- src/core/MainScoreBoard/components/LeagueInfo/index.tsx
+++ src/core/MainScoreBoard/components/LeagueInfo/index.tsx
@@ -10,6 +10,7 @@ import { IAxiosData } from "@/models/api";
 import { useQuery } from "@tanstack/react-query";
 
 import LeagueInfoHeader from "./components/Header/LeagueInfoHeader";
+import TeamsCell from "./components/TeamsCell/TeamsCell";
 import { IFixtures } from "./models/IFixtures";
 
 const StyledContainer = styled("div")`
@@ -334,77 +335,5 @@ export default function LeagueInfo() {
                     </StyledTable>
                </StyledContainer>
           </>
-     );
-}
-
-interface ITeamsCellProps {
-     home: {
-          name: string;
-          logo: string;
-     };
-     away: {
-          name: string;
-          logo: string;
-     };
-}
-function TeamsCell(props: ITeamsCellProps) {
-     const { away, home } = props;
-     return (
-          <td
-               style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    // border: "2px solid red",
-                    padding: "5px",
-                    gap: 5,
-                    // fontSize: "10px"
-                    fontSize: "clamp(0.7rem, 1.5vw, 1.2rem)"
-               }}
-          >
-               <span
-                    style={{
-                         display: "flex",
-                         gap: 20
-                    }}
-               >
-                    <span
-                         style={{
-                              width: "20px"
-                         }}
-                    >
-                         <img
-                              style={{
-                                   width: "100%"
-                              }}
-                              src={home.logo}
-                              alt=""
-                         />
-                    </span>
-                    <span>{home.name}</span>
-               </span>
-
-               <span
-                    style={{
-                         display: "flex",
-                         gap: 20
-                    }}
-               >
-                    <span
-                         style={{
-                              width: "20px"
-                         }}
-                    >
-                         <img
-                              style={{
-                                   width: "100%"
-                              }}
-                              src={away.logo}
-                              alt=""
-                         />
-                    </span>
-
-                    <span>{away.name}</span>
-               </span>
-          </td>
      );
 }
