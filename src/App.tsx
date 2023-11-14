4621cfa8857556ef6601e1cdb969ddb8e8c040a2
Merhan Mustafov <m.o.mustafov@gmail.com>
2023-11-14 23:06:02 +0200

    dummy: Redux counter slice

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
+import { useDispatch, useSelector } from "react-redux";
+
+import styled from "styled-components";
+
+import { decrement, increment, reset } from "@/global/slices/counterSlice";
+import { AppDispatch, RootState } from "@/global/store";
+
+const StyledButtoonsWrapper = styled("div")`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+const StyledButton = styled("button")`
+  cursor: pointer;
+`;
+
 function App() {
-  return <div>InSport</div>;
+  const dispatch: AppDispatch = useDispatch();
+  const count = useSelector((state: RootState) => state.counter.count);
+  const onIncrement = () => {
+    dispatch(increment());
+  };
+  const onDecrement = () => {
+    dispatch(decrement());
+  };
+  const onReset = () => {
+    dispatch(reset());
+  };
+  return (
+    <div>
+      <StyledButtoonsWrapper>
+        <StyledButton onClick={onIncrement}>Increment</StyledButton>
+        <StyledButton onClick={onDecrement}>Decrement</StyledButton>
+        <StyledButton onClick={onReset}>Reset</StyledButton>
+        <h1>Result: {count}</h1>
+      </StyledButtoonsWrapper>
+    </div>
+  );
 }
 
 export default App;
--- src/global/store.ts
+++ src/global/store.ts
@@ -1,7 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import counterSlice from "@/global/slices/counterSlice";
+
 export const store = configureStore({
-  reducer: {}
+  reducer: {
+    counter: counterSlice
+  }
 });
 
 export type RootState = ReturnType<typeof store.getState>;
