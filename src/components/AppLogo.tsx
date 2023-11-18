1ced824673b4407240f80a8a8ccf8608fecb7c9f
Merhan Mustafov <m.o.mustafov@gmail.com>
2023-11-18 18:23:26 +0200

    IN-32 FIX: moved app logo in a separate component.

---
+++ src/components/AppLogo.tsx
@@ -0,0 +1,49 @@
+import { useNavigate } from "react-router-dom";
+
+import styled from "styled-components";
+
+import { LANDING } from "@/router/pathConsts";
+
+const StyledLogoWrapper = styled.div`
+  grid-area: APP_NAV_LOGO;
+  cursor: pointer;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  gap: 0.3rem;
+  font-size: 3rem;
+  padding: 2px 15px;
+
+  &:hover {
+    box-shadow: #ffffff 0px 0px 10px -2px;
+  }
+`;
+const StyledWord = styled.span`
+  letter-spacing: 0.3rem;
+  font-family: "Courier New", Courier, monospace;
+`;
+
+const StyledIN = styled(StyledWord)`
+  color: #fff;
+  font-style: italic;
+`;
+
+const StyledSPORT = styled(StyledWord)`
+  color: #ff0040;
+  font-weight: 600;
+`;
+
+export default function AppLogo() {
+  const navigate = useNavigate();
+
+  const onLogoClick = () => {
+    navigate(LANDING);
+  };
+
+  return (
+    <StyledLogoWrapper onClick={onLogoClick}>
+      <StyledIN>In</StyledIN>
+      <StyledSPORT>Sport</StyledSPORT>
+    </StyledLogoWrapper>
+  );
+}
--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,8 +1,9 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import styled from "styled-components";
 
-import { LANDING, SOCCER } from "@/router/pathConsts";
+import AppLogo from "@/components/AppLogo";
+import { SOCCER } from "@/router/pathConsts";
 
 interface AppLayoutProps {
   children: React.ReactElement;
@@ -11,18 +12,17 @@ interface AppLayoutProps {
 const StyledAppLayout = styled.div`
   display: grid;
   flex-direction: column;
-  grid-template-columns: auto;
-  grid-template-rows: max-content auto;
+  grid-template-columns: 1fr;
+  grid-template-rows: max-content 1fr;
   grid-template-areas:
-    "HEADER"
-    "MAIN";
-  width: 100vw;
+    "APP_HEADER"
+    "APP_MAIN";
   height: 100vh;
 `;
 
 const StyledAppLayoutHeader = styled.div`
   /* border: 2px solid blue; */
-  grid-area: HEADER;
+  grid-area: APP_HEADER;
   background: #001e28;
   display: flex;
   flex-direction: row;
@@ -36,7 +36,7 @@ const StyledNav = styled.nav`
   align-items: center;
   grid-template-columns: max-content auto max-content;
   grid-template-rows: auto;
-  grid-template-areas: "NAV-LOG . NAV-LINKS";
+  grid-template-areas: "APP_NAV_LOGO . APP_NAV_LINKS";
   width: 100%;
   max-width: 1900px;
   background: transparent;
@@ -46,15 +46,8 @@ const StyledNav = styled.nav`
   font-size: 2rem;
 `;
 
-const StyledLogoWrapper = styled.div`
-  /* border: 2px solid black; */
-  grid-area: NAV-LOG;
-  cursor: pointer;
-  font-size: 3rem;
-`;
-
 const StyledUL = styled.ul`
-  grid-area: NAV-LINKS;
+  grid-area: APP_NAV_LINKS;
   display: flex;
   flex-direction: row;
   gap: 10px;
@@ -70,14 +63,15 @@ const StyledLink = styled(Link)`
 `;
 
 const StyledAppLayoutMain = styled.div`
-  grid-area: MAIN;
+  border: 2px solid blue;
+  grid-area: APP_MAIN;
   display: flex;
   flex-direction: row;
   justify-content: center;
 `;
 
 const StyledContent = styled.div`
-  /* border: 2px solid blue; */
+  border: 2px solid blue;
   width: 100%;
   height: auto;
   max-width: 1900px;
@@ -85,16 +79,11 @@ const StyledContent = styled.div`
 `;
 
 export default function AppLayout({ children }: AppLayoutProps) {
-  const navigate = useNavigate();
-
-  const onLogoClick = () => {
-    navigate(LANDING);
-  };
   return (
     <StyledAppLayout>
       <StyledAppLayoutHeader>
         <StyledNav>
-          <StyledLogoWrapper onClick={onLogoClick}>InSport</StyledLogoWrapper>
+          <AppLogo />
           <StyledUL>
             <StyledLinkWrapper>
               <StyledLink to={SOCCER}>Soccer</StyledLink>
