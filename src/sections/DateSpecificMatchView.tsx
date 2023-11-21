6b1bd660eda75dee69d5d115ab60436f786a2c81
Merhan Mustafov <m.o.mustafov@gmail.com>
2023-11-21 04:23:56 +0200

    draft: add better version of the calendar. The actual functionality is yeat to be implemented.

--- src/sections/DateSpecificMatchView.tsx
+++ src/sections/DateSpecificMatchView.tsx
@@ -20,7 +20,17 @@ const StyledCalendarContent = styled.div`
   justify-items: stretch;
 `;
 
-const StyledCalendarCell = styled.div`
+const StyledCalendarCell = styled("div")`
+  &.selected-date {
+    color: #ffffff;
+    background: #000000;
+  }
+  &.empty-day {
+    color: #ffffff;
+    pointer-events: none;
+    background: #f3f3f3;
+  }
+  cursor: pointer;
   border: 1px solid #000000;
   display: flex;
   justify-content: center;
@@ -53,86 +63,83 @@ const weekDays = {
 };
 
 export default function DateSpecificMatchView() {
-  const arrayOfMonths = getMonthsInYear(1998);
+  const [selectedYear, setSelectedYear] = useState(2022);
+  const [selectedMonth, setSelectedMonth] = useState<keyof typeof months>(1);
+  const [selectedDay, setSelectedDay] = useState<number | null>(-1);
+  // const arrayOfMonths = getMonthsInYear(2023);
 
-  function getMonthsInYear(year: number) {
-    const _date = new Date(year, 0, 1);
-    const monthsInYear = [];
-    while (_date.getFullYear() === year) {
-      const arrayOfDayInMonth = getDaysInMonth(year, _date.getMonth());
-      monthsInYear.push({
-        year,
-        monthLong: months[(_date.getMonth() + 1) as keyof typeof months],
-        monthNumber: _date.getMonth() + 1,
-        monthIndex: _date.getMonth(),
-        days: arrayOfDayInMonth,
-        lastDay: arrayOfDayInMonth[arrayOfDayInMonth.length - 1],
-        firstDay: arrayOfDayInMonth[0]
-      });
-      _date.setMonth(_date.getMonth() + 1);
+  function getFirstWeekEmptyDays(year: number, month: number) {
+    const _date = new Date(year, month, 1);
+    if (_date.getDay() === 0) {
+      return Array.from(Array(7 - 1).keys());
     }
-    return monthsInYear;
+    return Array.from(Array(_date.getDay() - 1).keys());
+  }
+
+  function getLastWeekEmptyDays(year: number, month: number, days: number) {
+    const _date = new Date(year, month, days);
+    if (_date.getDay() === 0) {
+      return [];
+    }
+    return Array.from(Array(7 - _date.getDay()).keys());
   }
 
   function getDaysInMonth(year: number, month: number) {
     const _date = new Date(year, month, 1);
 
     const daysInMonth = [];
+    let totalDay = 0;
+
     while (_date.getMonth() === month) {
       daysInMonth.push(_date.getDate());
+      totalDay += 1;
       _date.setDate(_date.getDate() + 1);
     }
+    getFirstWeekEmptyDays(year, month).forEach(() => daysInMonth.unshift(null));
+    getLastWeekEmptyDays(year, month, totalDay).forEach(() => daysInMonth.push(null));
+
     return daysInMonth;
   }
 
-  const getFirstWeekEmptyDays = (year: number, month: number) => {
-    const _date = new Date(year, month, 1);
-    if (_date.getDay() === 0) {
-      return Array.from(Array(6).keys());
-    }
-    return Array.from(Array(_date.getDay() - 1).keys());
-  };
+  const arrayOfDaysInMonth = getDaysInMonth(selectedYear, selectedMonth - 1);
 
-  const getLastWeekEmptyDays = (year: number, month: number, days: number) => {
-    const _date = new Date(year, month, days);
-    console.log(Array.from(Array(_date.getDay()).keys()));
-    if (_date.getDay() === 0) {
-      return [];
+  const onDateClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const day = Number((e.target as HTMLDivElement).id);
+    console.log(day);
+    if (day) {
+      setSelectedDay(day);
+      return;
     }
-    return Array.from(Array(7 - _date.getDay()).keys());
+    setSelectedDay(-1);
   };
 
   return (
     <StyledAllCalendarsWrapper>
-      {arrayOfMonths.map((data) => (
-        <StyledCalendarWrapper key={`#${Math.random() * 100 * Math.random()}`}>
-          <StyledCalendarHeader>
-            <div>
-              {data.year} {data.monthLong}
-            </div>
-          </StyledCalendarHeader>
+      <StyledCalendarWrapper key={`#${Math.random() * 100 * Math.random()}`}>
+        <StyledCalendarHeader>
+          <div>
+            {selectedYear} {months[selectedMonth]}
+          </div>
+        </StyledCalendarHeader>
 
-          <StyledCalendarContent>
-            {Object.values(weekDays).map((d) => (
-              <StyledCalendarCell key={d}>{d}</StyledCalendarCell>
-            ))}
-            {data.days.map((day) => (
-              <>
-                {day === data.firstDay &&
-                  getFirstWeekEmptyDays(data.year, data.monthIndex).map((i) => (
-                    <StyledCalendarCell key={i}></StyledCalendarCell>
-                  ))}
-                <StyledCalendarCell key={day}>{day}</StyledCalendarCell>
-
-                {day === data.lastDay &&
-                  getLastWeekEmptyDays(data.year, data.monthIndex, data.lastDay).map((i) => (
-                    <StyledCalendarCell key={i}></StyledCalendarCell>
-                  ))}
-              </>
-            ))}
-          </StyledCalendarContent>
-        </StyledCalendarWrapper>
-      ))}
+        <StyledCalendarContent>
+          {Object.values(weekDays).map((d) => (
+            <StyledCalendarCell key={d}>{d}</StyledCalendarCell>
+          ))}
+          {arrayOfDaysInMonth.map((day) => (
+            <StyledCalendarCell
+              onClick={(e) => onDateClick(e)}
+              id={`${day}`}
+              className={`${day === selectedDay ? "selected-date" : ""} ${
+                day === null ? "empty-day" : ""
+              }`}
+              key={day}
+            >
+              {day}
+            </StyledCalendarCell>
+          ))}
+        </StyledCalendarContent>
+      </StyledCalendarWrapper>
     </StyledAllCalendarsWrapper>
   );
 }
