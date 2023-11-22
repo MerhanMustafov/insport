5bae9e162bf34a0d900f65019143a11cfb2d2764
Merhan Mustafov <m.o.mustafov@gmail.com>
2023-11-22 03:11:10 +0200

    draft: Added lookUp objects for the days of the week and the months of the year

--- src/sections/DateSpecificMatchView.tsx
+++ src/sections/DateSpecificMatchView.tsx
@@ -6,6 +6,7 @@ const StyledAllCalendarsWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   row-gap: 100px;
+  font-family: "Courier New", Courier, monospace;
 `;
 const StyledCalendarWrapper = styled.div`
   font-size: 2rem;
@@ -21,6 +22,17 @@ const StyledCalendarContent = styled.div`
 `;
 
 const StyledCalendarCell = styled("div")`
+  cursor: pointer;
+  border: 1px solid #000000;
+  display: flex;
+  justify-content: center;
+  border: none;
+  padding: 5px 15px;
+  /* &.calendar___head_days {
+    color: #ffffff;
+    color: #000000;
+    border: none;
+  } */
   &.selected-date {
     color: #ffffff;
     background: #000000;
@@ -30,11 +42,6 @@ const StyledCalendarCell = styled("div")`
     pointer-events: none;
     background: #f3f3f3;
   }
-  cursor: pointer;
-  border: 1px solid #000000;
-  display: flex;
-  justify-content: center;
-  padding: 5px 15px;
 `;
 
 const months = {
@@ -62,26 +69,45 @@ const weekDays = {
   7: "Sun"
 };
 
+const daysLookUp = {
+  1: 1,
+  2: 2,
+  3: 3,
+  4: 4,
+  5: 5,
+  6: 6,
+  0: 7
+};
+
+const monthsLookUp = {
+  1: 0,
+  2: 1,
+  3: 2,
+  4: 3,
+  5: 4,
+  6: 5,
+  7: 6,
+  8: 7,
+  9: 8,
+  10: 9,
+  11: 10,
+  12: 11
+};
 export default function DateSpecificMatchView() {
-  const [selectedYear, setSelectedYear] = useState(2022);
-  const [selectedMonth, setSelectedMonth] = useState<keyof typeof months>(1);
+  const [selectedYear, setSelectedYear] = useState(2010);
+  const [selectedMonth, setSelectedMonth] = useState<keyof typeof monthsLookUp>(6);
   const [selectedDay, setSelectedDay] = useState<number | null>(-1);
-  // const arrayOfMonths = getMonthsInYear(2023);
 
   function getFirstWeekEmptyDays(year: number, month: number) {
     const _date = new Date(year, month, 1);
-    if (_date.getDay() === 0) {
-      return Array.from(Array(7 - 1).keys());
-    }
-    return Array.from(Array(_date.getDay() - 1).keys());
+    const day = _date.getDay() as keyof typeof daysLookUp;
+    return Array.from(Array(daysLookUp[day] - 1).keys());
   }
 
   function getLastWeekEmptyDays(year: number, month: number, days: number) {
     const _date = new Date(year, month, days);
-    if (_date.getDay() === 0) {
-      return [];
-    }
-    return Array.from(Array(7 - _date.getDay()).keys());
+    const day = _date.getDay() as keyof typeof daysLookUp;
+    return Array.from(Array(7 - daysLookUp[day]).keys());
   }
 
   function getDaysInMonth(year: number, month: number) {
@@ -101,7 +127,7 @@ export default function DateSpecificMatchView() {
     return daysInMonth;
   }
 
-  const arrayOfDaysInMonth = getDaysInMonth(selectedYear, selectedMonth - 1);
+  const arrayOfDaysInMonth = getDaysInMonth(selectedYear, monthsLookUp[selectedMonth]);
 
   const onDateClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     const day = Number((e.target as HTMLDivElement).id);
@@ -124,7 +150,9 @@ export default function DateSpecificMatchView() {
 
         <StyledCalendarContent>
           {Object.values(weekDays).map((d) => (
-            <StyledCalendarCell key={d}>{d}</StyledCalendarCell>
+            <StyledCalendarCell className="calendar___head_days" key={d}>
+              {d}
+            </StyledCalendarCell>
           ))}
           {arrayOfDaysInMonth.map((day) => (
             <StyledCalendarCell
